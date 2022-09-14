118452d541920fbe41582b7400a2afaccbb266f6
okutyann <h19981029@gmail.com>
2022-09-14 21:25:17 +0900

    ✨ PHPでselect構文を実行

---
+++ sample02.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sample02</title>
+</head>
+
+<body>
+    <?php
+    $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
+    $records = $db->query('select * from my_item');
+    // 中身の表示
+    // var_dump($records);
+    if ($records) {
+        // fetch_assoc = fetch データを１件取り出すよの意味
+        //             = assoc アソシエイト　連想配列として取り出すよ 
+        while ($record = $records->fetch_assoc()) {
+            echo $record['item_name'] . '<br>';
+        }
+    } else {
+        echo $db->error;
+    }
+    ?>
+
+</body>
+
+</html>

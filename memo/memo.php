14c2fea3d4ab3ea0c4221e40608d05db0e275dd7
okutyann <h19981029@gmail.com>
2022-09-19 19:09:43 +0900

    🐛  一覧画面い移動しない修正

--- memo/memo.php
+++ memo/memo.php
@@ -36,7 +36,7 @@
     <p>
         <a href="update.php?id=<?php echo $id; ?>">編集する</a>|
         <a href="delete.php?id=<?php echo $id; ?>">削除する</a>|
-        <a href="/memo">一覧へ</a>
+        <a href="../memo">一覧へ</a>
     </p>
 </body>
 
